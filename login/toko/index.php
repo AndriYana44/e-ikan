f91cbbcbac83f0dc46326723d7cf278e4d816222
AndriYana44 <andri.yana349@gmail.com>
2021-01-13 16:14:22 +0700

    fix

--- login/toko/index.php
+++ login/toko/index.php
@@ -1,16 +1,21 @@
 <?php
-if ($_GET['halaman'] == 'vahicle') {
-    header('location: tambah_vahicle.php');
-} else if ($_GET['halaman'] == 'produk_kios') {
-    header('location: produk_kios.php');
-} else if ($_GET['halaman'] == 'produk_terjual') {
-    header('location: produk_terjual.php');
-} else if ($_GET['halaman'] == 'tambah_produk') {
-    header('location: tambah_produk.php');
-} else if ($_GET['halaman'] == 'tambah_artikel') {
-    header('location: tambah_artikel.php');
-} else if ($_GET['halaman'] == 'tambah_publikasi') {
-    header('location: tambah_publikasi.php');
-} else if ($_GET['halaman'] == 'pengaturan_akun') {
-    header('location: pengaturan_akun.php');
+session_start();
+if($_SESSION['alogin'] !== "login") {
+    header('location: ../index.php');
+}else {
+    if ($_GET['halaman'] == 'vahicle') {
+        header('location: tambah_vahicle.php');
+    } else if ($_GET['halaman'] == 'produk_kios') {
+        header('location: produk_kios.php');
+    } else if ($_GET['halaman'] == 'produk_terjual') {
+        header('location: produk_terjual.php');
+    } else if ($_GET['halaman'] == 'tambah_produk') {
+        header('location: tambah_produk.php');
+    } else if ($_GET['halaman'] == 'tambah_artikel') {
+        header('location: tambah_artikel.php');
+    } else if ($_GET['halaman'] == 'tambah_publikasi') {
+        header('location: tambah_publikasi.php');
+    } else if ($_GET['halaman'] == 'pengaturan_akun') {
+        header('location: pengaturan_akun.php');
+    }
 }
